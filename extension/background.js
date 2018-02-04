c321347b38fb518c0b54d2fc7b66f7b63ae21dd5
pjheden <julianheden@gmail.com>
2018-02-04 16:36:36 +0100

    Beutify project

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,10 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, { file: "jquery-3.3.1.min.js" }, function() {
-      chrome.tabs.executeScript(null, { file: "inject.js" });
+  chrome.tabs.executeScript(null, {
+    file: "jquery-3.3.1.min.js"
+  }, function() {
+    chrome.tabs.executeScript(null, {
+      file: "inject.js"
+    });
   });
 });
--- extension/inject.js
+++ extension/inject.js
@@ -1,1 +1,4 @@
-console.log("im running!");var section = $('h4[data-a-target="international-section-header"]').parent().parent().last();section.css('visibility', 'hidden');section.css('width', '0px');section.css('height', '0px');
+var section = $('h4[data-a-target="international-section-header"]').parent().parent().last();
+section.css('visibility', 'hidden');
+section.css('width', '0px');
+section.css('height', '0px');
