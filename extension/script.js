8ea981a3116f70fb79e826ab6fac5517794573c0
pjheden <julianheden@gmail.com>
2018-02-04 14:39:15 +0100

    Find and hide element

--- extension/script.js
+++ extension/script.js
@@ -1,1 +1,14 @@
 console.log("im running!");
+
+window.onload = function() {
+  console.log("im running onload!");
+  setTimeout(function() {
+    console.log("im running timeout!");
+
+    var section = $('h4[data-a-target="international-section-header"]').parent().parent().last();
+    section.css('visibility', 'hidden');
+    section.css('width', '0px');
+    section.css('height', '0px');
+  }, 1000)
+}
+// data-a-target='international-section-header'
