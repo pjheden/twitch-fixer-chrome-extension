9a4ca5a85a9c25669b0a3b9ca2a3627c705b0b97
pjheden <julianheden@gmail.com>
2018-02-04 16:25:19 +0100

    Add onclick listener

---
+++ extension/background.js
@@ -0,0 +1,6 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(null, { file: "jquery-3.3.1.min.js" }, function() {
+      chrome.tabs.executeScript(null, { file: "inject.js" });
+  });
+});
---
+++ extension/inject.js
@@ -0,0 +1,1 @@
+console.log("im running!");var section = $('h4[data-a-target="international-section-header"]').parent().parent().last();section.css('visibility', 'hidden');section.css('width', '0px');section.css('height', '0px');
